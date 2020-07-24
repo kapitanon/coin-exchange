1c64a639b5861bdf3871168d8c7062024040c1d5
kapitanon <theysayjustdoingmyjob@protonmail.com>
2020-07-24 03:47:40 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Coin from './components/Coin/Coin';
+import AccountBalance from './components/AccountBalance/AccountBalance';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} alt="react-logo" className="App-logo"/>
+        <h1 className="App-title">
+          Coin Exchange
+        </h1>
+      </header>
+      < AccountBalance amount={10000} />
+      <table className="coin-table">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Ticker</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          <Coin name="Bitcoin" ticker="BTC" price={9999.99} />
+          <Coin name="Ethereum" ticker="ETH" price={299.99} />
+          <Coin name="Tether" ticker="USDT" price={1.00} />
+          <Coin name="Ripple" ticker="XRP" price={.25} />
+
+
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default App;
